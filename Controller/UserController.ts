83167e7e40470147d0fabbadc9a9bda5448aa12e
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-22 15:07:28 +0100

    updated the controller

--- Controller/UserController.ts
+++ Controller/UserController.ts
@@ -1,11 +1,28 @@
 import { Request,Response } from "express";
 import UserModel from "../Model/User.Model";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
+
+// Register User
 export const RegisterUser = async(req:Request,res:Response)=>{
 
     try {
+        const {name,email,password,userName,phoneNumber} = req.body;
+
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password,salt)
+
         
+
+        const generateNumber = Math.floor(Math.random()* 9876543210) 
+
+        const register = await UserModel.create({
+            name,email,userName,password:hash,phoneNumber,verified:true,accountNumber: generateNumber
+        })
     } catch (error) {
-        
+        return res.status(404).json({
+            message:"An Error Occurred",error
+        })
     }
 }
